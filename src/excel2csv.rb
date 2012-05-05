8a73f93138543c7d3cb599391477d82f4495cde5
Stephen Pike <spike@scpike.net>
2012-05-05 14:36:01 -0400

    Initial commit

---
+++ src/excel2csv.rb
@@ -0,0 +1,45 @@
+# encoding: UTF-8
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'roo'
+
+
+USAGE = <<END
+Convert an excel file to csv via the roo library
+
+Example usage:
+# Print the csv to std out
+  excel2csv file.xls
+
+# Write the csv to file
+  excel2csv file.xls out.csv
+
+END
+
+def die_with_usage(msg=nil)
+  puts USAGE
+  puts msg if msg
+  exit
+end
+
+filename = ARGV[0]
+
+die_with_usage "You must pass a file." unless filename
+
+if filename =~ /xlsx$/
+  excel = Excelx.new(filename)
+else
+  excel = Excel.new(filename)
+end
+
+output = STDOUT
+if ARGV[1]
+  output = File.open(ARGV[1], "w")
+end
+
+1.upto(excel.last_row) do |line|
+  output.write CSV.generate_line excel.row(line)
+end
+
+
